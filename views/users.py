578578c83f8823e2f18999d4a49770d3e9ab25f1
Vladislav <xxx.swat.ru@gmail.com>
2022-10-04 15:56:27 +0300

    add and fix: user_ns = Namespace('users') api.add_namespace(auth_services) username = db.Column(db.String, unique=True)

--- views/users.py
+++ views/users.py
@@ -4,7 +4,7 @@ from flask_restx import Resource, Namespace
 from dao.model.user import UserSchema
 from implemented import user_service
 
-user_ns = Namespace('movies')
+user_ns = Namespace('users')
 
 
 @user_ns.route('/')
